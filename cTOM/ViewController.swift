416cdc1a112a66d0464b768ef31bbd9d8bf8131f
Conor O'Grady <conorogrady@conors-macbook-pro.local>
2018-01-29 19:55:41 +0000

    Initial Commit

---
+++ cTOM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cTOM
+//
+//  Created by Conor O'Grady on 29/01/2018.
+//  Copyright © 2018 Conor O'Grady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

12a369b1c0521986f9f1a85d7e2c8a99a46d2006
Conor O'Grady <conorogrady@conors-macbook-pro.local>
2018-02-13 18:41:25 +0000

    login and register page for Admin

---
+++ cTOM/ViewControllerAdminLogin.swift
@@ -0,0 +1,76 @@
+//
+//  ViewControllerAdminLogin.swift
+//  cTOM
+//
+//  Created by Conor O'Grady on 12/02/2018.
+//  Copyright © 2018 Conor O'Grady. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerAdminLogin: UIViewController {
+    
+    @IBOutlet weak var errorMessage: UILabel!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    @IBAction func loginAction(_ sender: UIButton) {
+        
+        if emailField.text! == "" {
+            createAlert(title: "Error", message: "Email Field Is Empty!")
+        } else if passwordField.text! == "" {
+            createAlert(title: "Error", message: "Password Field Is Empty!")
+        } else if DBManager.checkEmailAndPasswordCorrect(email: emailField.text!, password: passwordField.text!) == true {
+            Trackers.adminLoggedIn = true
+            Trackers.currentAdmin = DBManager.getCurrentAdmin(email: emailField.text!)
+            self.performSegue(withIdentifier: "successfulLogin", sender: self)  
+        } else {
+            errorMessage.isHidden = false
+        }
+    }
+    // logic for when admin tries to login i.e. what alerts should display first etc.
+    
+    func createAlert (title: String, message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    // function to create alert
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        errorMessage.isHidden = true
+        
+        // Checks to see if .db file exists in documents directory and copies from main bundle if needed
+        DBManager.copyDatabaseIfNeeded()
+        
+        // Opens DB
+        DBManager.openDatabase()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
